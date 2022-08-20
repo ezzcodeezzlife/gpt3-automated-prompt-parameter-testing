73b12dc65961838f2583018c66ee6da6df80bd29
ezzcodeezzlife <fabian.stehle@informatik.hs-fulda.de>
2022-08-20 12:45:35 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
